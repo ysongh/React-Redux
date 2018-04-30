20e3b4c2e861fe4c337126f0af5b72df89f09b47
Song <yousonghou@yahoo.com>
2018-04-30 03:23:14 +0000

    Version 3

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -32,7 +32,7 @@ const mapStateToProps = state => {
 // to get access to dispatch functions
 const mapDispatchToProps = dispatch => {
     return{
-        onAddedPerson: () => dispatch({type: actionTypes.ADD_PERSON}),
+        onAddedPerson: (name, age) => dispatch({type: actionTypes.ADD_PERSON, personData: {name: name, age: age}}),
         onRemovedPerson: (id) => dispatch({type: actionTypes.REMOVE_PERSON, personId: id})
     };
 };
